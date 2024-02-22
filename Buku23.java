f782c334fd376d24bf34c39272f1b521b196fe7b
PurnamaRidzkyN <purnamanugraha492@gmail.com>
2024-02-22 12:58:47 +0700

    first commit

---
+++ Buku23.java
@@ -0,0 +1,32 @@
+public class Buku23 {
+    String judul;
+    String pengarang;
+    int halaman;
+    int stok;
+    int harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok>0){
+        stok = -jml;
+        }else {
+            System.out.println("Stok habis. Tidak dapat melakukan penjualan.");
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+
+}
